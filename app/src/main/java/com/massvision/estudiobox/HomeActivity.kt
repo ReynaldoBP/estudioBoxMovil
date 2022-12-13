79ff50a633446096c58266aff54f0bd31713b5ba
Kevin Baque <baquekevin@hotmail.com>
2022-12-13 00:46:28 -0500

    Autenticacion de usuario

---
+++ app/src/main/java/com/massvision/estudiobox/HomeActivity.kt
@@ -0,0 +1,46 @@
+package com.massvision.estudiobox
+
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_home.*
+
+enum class ProviderType
+{
+    BASIC,
+    GOOGLE
+}
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+        //Setup
+        //Logica para ingresar por correo-firebase
+        val bundle= intent.extras
+        val email = bundle?.getString("email")
+        val provider = bundle?.getString("provider")
+        setup(email?:"",provider?:"")
+        //Guardar la sesión del usuario autenticado
+        val prefs = getSharedPreferences(getString(R.string.prefs_file), Context.MODE_PRIVATE).edit()
+        prefs.putString("email",email)
+        prefs.putString("provider",provider)
+        prefs.apply()
+    }
+    private fun setup(email:String,provider:String)
+    {
+        title = "Inicio"
+        emailtextView.text = email
+        providertextView.text = provider
+        logOutButton.setOnClickListener {
+            //Borrado de datos
+            val prefs = getSharedPreferences(getString(R.string.prefs_file),Context.MODE_PRIVATE).edit()
+            prefs.clear()
+            prefs.apply()
+            //Cerrar session
+            FirebaseAuth.getInstance().signOut()
+            onBackPressed()//volver a la pantalla anterior
+        }
+    }
+}

f6e26796e72f55b408c0667f2c07270bf51957d8
kbaque <baquekevin@hotmail.com>
2023-01-03 18:19:52 -0500

    Respaldo 03 dic

--- app/src/main/java/com/massvision/estudiobox/Repository/ApiService.kt
+++ app/src/main/java/com/massvision/estudiobox/Repository/ApiService.kt
@@ -3,6 +3,7 @@ import com.google.gson.JsonObject
 import com.massvision.estudiobox.Model.EmpresaDataCollectionItem
 import com.massvision.estudiobox.Model.EncuestaDataCollectionItem
 import com.massvision.estudiobox.Model.PreguntaDataCollectionItem
+import com.massvision.estudiobox.Model.RespuestaDataCollectionItem
 import org.json.JSONObject
 import retrofit2.Response
 import retrofit2.http.Body
@@ -15,4 +16,6 @@ interface ApiService {
     suspend fun getEncuesta(@Body jsonObject: JsonObject):Response<EncuestaDataCollectionItem>
     @POST("/apiMovil/getPregunta")
     suspend fun getPregunta(@Body jsonObject: JsonObject):Response<PreguntaDataCollectionItem>
+    @POST("/apiMovil/createRespuesta")
+    suspend fun createRespuesta(@Body jsonObject: JsonObject):Response<RespuestaDataCollectionItem>
 }
--- app/src/main/java/com/massvision/estudiobox/View/PreguntaActivity.kt
+++ app/src/main/java/com/massvision/estudiobox/View/PreguntaActivity.kt
@@ -1,21 +1,21 @@
 package com.massvision.estudiobox.View
 
-import android.content.Intent
 import android.graphics.Color
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.InputType
 import android.util.Log
 import android.view.Gravity.CENTER
 import android.widget.*
-
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.addTextChangedListener
 import androidx.lifecycle.lifecycleScope
 import com.google.gson.JsonObject
 import com.massvision.estudiobox.R
 import com.massvision.estudiobox.Repository.ApiService
 import com.massvision.estudiobox.Repository.RetrofitHelper
-import kotlinx.android.synthetic.main.activity_empresa.*
+import kotlinx.android.synthetic.main.activity_login.*
 import kotlinx.android.synthetic.main.activity_pregunta.*
-import org.json.JSONObject
+
 
 class PreguntaActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -60,43 +60,95 @@ class PreguntaActivity : AppCompatActivity() {
                     )
                     if (response.body()?.intStatus == 200 && response.body()?.arrayPregunta?.isNotEmpty() == true) {
                         Log.d("Interceptor", "+++++++++++++++")
+                        val jsonPregunta = JsonObject()
+                        //Recorremos las preguntas
                         for (arrayItem in response.body()?.arrayPregunta!!) {
                             Log.d("Interceptor", "Pregunta: " + arrayItem.strPregunta)
+                            //Presentamos las preguntas
                             val button = Button(this@PreguntaActivity)
                             button.setLayoutParams(buttonLayout)
                             button.setText(arrayItem.strPregunta)
                             button.setBackgroundColor(Color.TRANSPARENT)
                             button.setTextColor(Color.BLACK)
                             preguntaLayoutSecundario.addView(button)
-
-                            val ratingBar: RatingBar = RatingBar(this@PreguntaActivity).apply {
-                                setIsIndicator(false)
-                                numStars = arrayItem.intCantidadEstrellas
-                                stepSize = 1.0f
-                                layoutParams = LinearLayout.LayoutParams(
-                                    LinearLayout.LayoutParams.WRAP_CONTENT,
-                                    LinearLayout.LayoutParams.WRAP_CONTENT
-                                ).apply { gravity = CENTER }
+                            //Presentamos las estrellas
+                            if (arrayItem.strTipoOpcionRespuesta == "CERRADA")
+                            {
+                                val ratingBar: RatingBar = RatingBar(this@PreguntaActivity).apply {
+                                    id = arrayItem.intIdPregunta
+                                    setIsIndicator(false)
+                                    numStars = arrayItem.intCantidadEstrellas
+                                    stepSize = 1.0f
+                                    layoutParams = LinearLayout.LayoutParams(
+                                        LinearLayout.LayoutParams.WRAP_CONTENT,
+                                        LinearLayout.LayoutParams.WRAP_CONTENT
+                                    ).apply { gravity = CENTER }
+                                }
+                                ratingBar.onRatingBarChangeListener =
+                                    RatingBar.OnRatingBarChangeListener { _, rating, _ ->
+                                        jsonPregunta.addProperty(
+                                            ratingBar.id.toString(),
+                                            rating.toInt()
+                                        )
+                                        Toast.makeText(
+                                            this@PreguntaActivity, "Id:" + ratingBar.id + " Stars: " +
+                                                    rating.toInt(), Toast.LENGTH_SHORT
+                                        ).show()
+                                    }
+                                //ratingBar.progressBackgroundTintList(Color.BLUE)
+                                preguntaLayoutSecundario.addView(ratingBar)
                             }
-                            //ratingBar.progressBackgroundTintList(Color.BLUE)
-
-                            preguntaLayoutSecundario.addView(ratingBar)
+                            //Presentamos texto para las preguntas con comentarios
+                            else
+                            {
+                                val text = EditText(this@PreguntaActivity)
+                                text.id = arrayItem.intIdPregunta
+                                text.addTextChangedListener {
+                                    jsonPregunta.addProperty(
+                                        text.id.toString(),
+                                        text.getText().toString()
+                                    )
+                                }
+                                preguntaLayoutSecundario.addView(text)
+                            }
                             val space = Space(this@PreguntaActivity)
                             space.setLayoutParams(spaceLayout)
                             preguntaLayoutSecundario.addView(space)
                             Log.d("Interceptor", "----------------------")
                         }
+                        val jsonDataRespuesta = JsonObject()
+                        jsonDataRespuesta.addProperty("intIdEncuesta",idEncuesta)
+                        jsonDataRespuesta.add("arrayPregunta",jsonPregunta)
+                        //Datos Adicionales
+                        val buttonSeparador = Button(this@PreguntaActivity)
+                        buttonSeparador.setLayoutParams(buttonLayout)
+                        buttonSeparador.setText("Datos Adicionales")
+                        buttonSeparador.setBackgroundColor(Color.TRANSPARENT)
+                        buttonSeparador.setTextColor(Color.BLACK)
+                        preguntaLayoutSecundario.addView(buttonSeparador)
+                        //Correo electronico del encuestado
+                        val textCorreo = EditText(this@PreguntaActivity)
+                        textCorreo.setInputType(InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_EMAIL_ADDRESS)
+                        textCorreo.setHint("Correo Electrónico")
+                        textCorreo.addTextChangedListener {
+                            jsonDataRespuesta.addProperty(
+                                "strCorreo",
+                                textCorreo.getText().toString()
+                            )
+                        }
+                        preguntaLayoutSecundario.addView(textCorreo)
+                        //Guardar Encuesta
                         val buttonGuardarEncuesta = Button(this@PreguntaActivity)
                         buttonGuardarEncuesta.setLayoutParams(buttonLayout)
                         buttonGuardarEncuesta.setText("Guardar Encuesta")
                         buttonGuardarEncuesta.setBackgroundColor(Color.BLUE)
                         buttonGuardarEncuesta.setTextColor(Color.WHITE)
                         buttonGuardarEncuesta.setOnClickListener {
-                            Toast.makeText(
-                                this@PreguntaActivity,
-                                "Encuesta Guardada",
-                                Toast.LENGTH_LONG
-                            ).show()
+                            jsonDataRespuesta.addProperty("strGenero","FEMENINO")
+                            jsonDataRespuesta.addProperty("strEdad","1975-07-23")
+                            val jsonObjectRespuesta = JsonObject()
+                            jsonObjectRespuesta.add("data",jsonDataRespuesta)
+                            createRespuesta(jsonObjectRespuesta)
                         }
                         preguntaLayoutFooter.addView(buttonGuardarEncuesta)
                     } else {
@@ -117,5 +169,16 @@ class PreguntaActivity : AppCompatActivity() {
                 Log.e("Error", Ex.localizedMessage)
             }
         }
+    }
+    private fun createRespuesta(jsonObject:JsonObject)
+    {
+        Log.d("Interceptor", "Request Respuesta: " + jsonObject)
+        Toast.makeText(
+            this@PreguntaActivity,
+            "Encuesta Guardada",
+            Toast.LENGTH_LONG
+        ).show()
+        /*val apiService = RetrofitHelper.getInstance().create(ApiService::class.java)
+        val response = apiService.createRespuesta(jsonObject)*/
     }
 }
